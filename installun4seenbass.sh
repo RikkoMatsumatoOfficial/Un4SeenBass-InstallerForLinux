905e29850e7efe396a4ebbef7de9bece0343e27e
Rikko Matsumato <kuttimsx@hotmail.com>
2025-06-05 13:04:58 +0300

    Update installun4seenbass.sh

--- installun4seenbass.sh
+++ installun4seenbass.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo clear
-cp libbass.so /usr/local/lib
+sudo cp libbass.so /usr/local/lib
 echo "/usr/local/lib" > freeverb.conf
 sudo cp freeverb.conf /etc/ld.so.conf.d
 sudo ldconfig
